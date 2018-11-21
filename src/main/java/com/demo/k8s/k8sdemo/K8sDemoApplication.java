dff5ebae1522e5c607852bf1551cf08ef15d3e52
Semen Babenko <sbaben@softserveinc.com>
2018-11-21 15:37:23 +0200

    Add controller.

---
+++ src/main/java/com/demo/k8s/k8sdemo/K8sDemoApplication.java
@@ -0,0 +1,24 @@
+package com.demo.k8s.k8sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.Optional;
+
+@SpringBootApplication
+@RestController
+public class K8sDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sDemoApplication.class, args);
+	}
+
+	@GetMapping("/{name}")
+	public String hello(@PathVariable("name") String name) {
+		return String.format("Hello %s", name);
+	}
+}
